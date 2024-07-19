c5a26b30d80849a35fc9859b5c58847e075e0ad7
FelixFrankFelix <frankozie4life@gmail.com>
2024-07-19 13:19:35 +0100

    added user verification status to create bulk session

--- service.py
+++ service.py
@@ -2,7 +2,7 @@ from utility.exceptions import ResponseConstant
 import repository
 from typing import List
 from sqlalchemy.orm import Session
-from schemas import SessionRequest, Verification, VerificationUpdateRequest
+from schemas import SessionRequest, Verification, VerificationUpdateRequest, UserSessions
 import repository
 
 def create_session_service(body):
@@ -95,17 +95,24 @@ def update_user_ver_service(body):
         }
 
 
-def bulk_create_session_service(body,db):
+def bulk_create_session_service(db_sessions,db):
+    #print(db_sessions)
     count = 0
-    for session in body:
+    for session in db_sessions:
         repository.create_session(db=db,session=session)
         if session.sessionUserIsPresent == "YES":
             count += 1
     
-    user_id = body[0].sessionUserId
+    user_id = db_sessions[0].sessionUserId
     user_ver = repository.get_user_ver_by_id(db=db,user_id=user_id)
     new_user_ver = {}
 
+    # Convert database rows to dictionaries
+    sessions = [session.dict() for session in db_sessions]
+    user_ver = user_ver._asdict() if user_ver is not None else None
+    print(sessions,user_ver)
+    
+    # Create Pydantic model instance
     if not user_ver:
         new_user_ver = Verification(
             userId=user_id,
@@ -114,6 +121,8 @@ def bulk_create_session_service(body,db):
         )
         repository.create_user_ver(db=db, user_ver= new_user_ver)
 
+        body = UserSessions(sessions=sessions, user=new_user_ver)
+
         return {
         "responseCode": ResponseConstant.SUCCESS.responseCode,
         "responseMessage": ResponseConstant.SUCCESS.responseMessage, 
@@ -121,7 +130,7 @@ def bulk_create_session_service(body,db):
         }
         
     else: 
-        user_ver_dict = user_ver._asdict()
+        user_ver_dict = user_ver
         user_ver_dict["userVerificationCount"] += count
         if user_ver_dict["userVerificationCount"] > 8:
             user_ver_dict["userIsVerified"] = "YES"
@@ -131,6 +140,14 @@ def bulk_create_session_service(body,db):
             userIsVerified = user_ver_dict["userIsVerified"]
         )
         repository.update_user_ver(db=db,user_id=user_id,user_ver=update_data)
+
+        user_ver ={
+            "userId": user_id,
+            "userVerificationCount": user_ver_dict["userVerificationCount"],
+            "userIsVerified": user_ver_dict["userIsVerified"]
+        }
+        body = UserSessions(sessions=sessions, user=user_ver)
+
         return {
         "responseCode": ResponseConstant.SUCCESS.responseCode,
         "responseMessage": ResponseConstant.SUCCESS.responseMessage, 
